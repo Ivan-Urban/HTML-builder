4d4b6f9acdcf7de251b3f478ca4b341c9d5ee31d
Ivan-Urban <ivan.urbanovich.00@gmail.com>
2022-05-22 16:39:12 +0300

    feat: make 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const { copyFile, mkdir, readdir, unlink } = require('fs/promises');
+const path = require('path');
 
+mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
+
+readdir(path.join(__dirname, 'files-copy'), {withFileTypes: true}).then(res => {
+  res.forEach(el => {
+    unlink(path.join(__dirname, 'files-copy', el.name));
+  });
+});
+
+readdir(path.join(__dirname, 'files'), {withFileTypes: true}).then(res => {
+  res.forEach(el => {
+    copyFile(path.join(__dirname, 'files', el.name), path.join(__dirname, 'files-copy', el.name));  
+  });
+});
